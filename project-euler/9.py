955bc062b21ee5da21664674006dd2bc6a6ea2c4
Szmaragdynia <szmaragdynia@gmail.com>
2022-05-07 00:04:07 +0200

    work on 9

--- project-euler/9.py
+++ project-euler/9.py
@@ -7,13 +7,17 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the eproduct abc.
 '''
-
-def find_triplet(amount):
-    for i in range(1,100):
-        for j in range(1,100):
-            
+import math
 
 a = 0
 b = 0
 c = 0
+
+def find_triplet(amount):
+    for i in range(1,100):
+        for j in range(1,100):
+            if math.sqrt(i**2 + j**2).is_integer():
+                c = math.sqrt(i**2 + j**2)
+
+
 while (a+b+c < 1000):
