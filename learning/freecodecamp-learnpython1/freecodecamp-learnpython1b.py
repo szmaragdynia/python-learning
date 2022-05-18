05493994acd7fb880ea5482fdf8f6650c232040e
szmaragdynia <szmaragdynia@gmail.com>
2022-05-18 18:41:38 +0200

    Update freecodecamp-learnpython1b.py

--- learning/freecodecamp-learnpython1/freecodecamp-learnpython1b.py
+++ learning/freecodecamp-learnpython1/freecodecamp-learnpython1b.py
@@ -1,5 +1,4 @@
-##wydzielam nowy plik dla porzakdu jakiegos, w sensie zeby tak nie bylo nakichane wszystkiego do jednego
-
+#new file for retaining some kind of order in here. 
 imie = input("Hej, podaj imie: ")
 print("cześć " + imie)
 
@@ -10,5 +9,5 @@ numer2 = input("podaj numer2: ")
 
 print(numer1 + numer2)
 print(int(numer1)+int(numer2))
-    #to sie zesra przy nie-integerach
+    #it will break with non-integers
 print(float(numer1)+float(numer2))
