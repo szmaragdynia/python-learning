4f2a3b491675f840aa4c0c1368a01ad9353d5b84
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-08-02 00:23:15 +0200

    moved contents of second random file into micro.py

--- code_lab/micro.py
+++ code_lab/micro.py
@@ -116,6 +116,26 @@ def d():
   )
   # TO DO difference between math that does not need importing, the one that does need, and the numpy (e.g. np.abs and abs - are there any differences in another methods?)
 
+# ----------------------------------------------------------------
+def e():
+  number1 = input("Enter number1: ")
+  number2 = input("Enter number2: ")
+
+  print("number1 + number2 - string concat!:", number1 + number2)
+  #this breaks if floats are used
+  try:
+    print("int(number1)+int(number2):", int(number1)+int(number2))
+    print("int(number1+number2):", int(number1+number2))
+  except ValueError:
+    print("Cannot cast to int, omitting two print statements")
+  
+  #this works with floats
+  print("float(number1)+float(number2)",float(number1)+float(number2))
+  try:
+    print("float(number1+number2)",float(number1+number2))
+  except ValueError:
+    print(f"Cannot sensibly cast that string ({number1+number2}) to float")
+
 
 
 # ----------------------------------------------------------------
@@ -127,23 +147,32 @@ code_fragments = {
     'b': b,
     'c': c,
     'd': d,
+    'e': e,
   }
 # ----------------------------------------------------------------
 # Whenever adding new code above, you probably do not care about code below
 import sys
 def main():
   is_first_iteration = True
+  prev_choice = 'does_not_exist' # magic number - flag
   while True:
     if len(sys.argv) > 1 and is_first_iteration:
       choice = sys.argv[1].strip().lower()
       is_first_iteration = False
     else:
       print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-')
-      choice = input("Choose preferred code_fragment ('qwqw'(sic!) to quit): ").strip().lower()
+      choice = input("Choose preferred code_fragment ('qwqw' to quit, 'agag' for previous choice): ").strip().lower()
       print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-')
+
+
     if choice == 'qwqw':
-        break
-    if choice in code_fragments:
+      break
+    if choice == 'agag' and choice != 'does_not_exist': #previos choice does exist
+      choice = prev_choice
+
+    prev_choice = choice
+
+    if choice in code_fragments:  
       code_fragments[choice]()
     else:
         print("Invalid choice.")
