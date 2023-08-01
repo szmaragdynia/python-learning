33d7da10adfa7962c58be9d98b4d18d91a9c940d
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-08-01 17:53:52 +0200

    cleanup

--- code_lab/standardplot.py
+++ code_lab/standardplot.py
@@ -1,8 +1,8 @@
 
 
-import matplotlib.pyplot as mplt
-import numpy as nmp
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = nmp.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-mplt.plot(x, nmp.sin(x))       # Plot the sine of each x point
-mplt.show()                   # Display the plot
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
