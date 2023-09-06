289a23f5f150643704206df6eba9b5646d976025
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-09-06 22:23:29 +0200

    fixed serious and absolutely trivial and elementary-school math error

--- sensible_scripts/gpsDataToSpeed/auxiliary/speedCalculation.py
+++ sensible_scripts/gpsDataToSpeed/auxiliary/speedCalculation.py
@@ -3,6 +3,13 @@ from geopy import distance
 from auxiliary.utils import logger
 from auxiliary.constants import tab
 
+# Todo: 
+#   j+1 should be renamed to "2nd_after_last_true" or something
+#   current_now and prev should be renamed appropriately in cases, so their names match the situation
+#   calculate_speeds should be renamed to calculate_distance, since this is what it does exactly. It is speed only when calculated measure-by-measure.
+#       However then multiplying by m/s->km/h coefficient should be moved to other place (since however it will work, it is not semantically correct to keep it in distance calculation)
+
+
 # latitude and longitude are given each second (Be they fake or real)
 earth_radius_my_location_meters = 6365816 # 49.5 latitude north
 def _haversine(lat1, lon1, lat2, lon2, lib=math):
@@ -61,7 +68,8 @@ def calculateAndAssign(dictionaries_list):
             # 1: T T T T ... - in that case we just calculate distance between this and previous point (treating it as distance/s, since measures are taken every second) and append to this measure
             # 2: T T F F F F {T T} - in that case we have real speed before a sequence of False values after which there are two real values. This means we have real speed before and after sequence of fake values. In this case, we interpolate lineraly from one speed to the next, assiging the appropriate values to the fake measures.
             # 3: T T F F F F T F - in this case, we cannot interpolate 
-
+        
+        # Case 1
         if dictionaries_list[i]['original_data'] == True:
             latitude_now = dictionaries_list[i]['latitude_deg']
             longitude_now = dictionaries_list[i]['longitude_deg']
@@ -75,14 +83,18 @@ def calculateAndAssign(dictionaries_list):
             dictionaries_list[i]['speed_source'] = "[T->T->T...]"
             logger("{0}[T->T->T...]".format(tab),stream="fileOnly")
             logger("{0}{1}".format(tab,dictionaries_list[i]['datetimeISO8601']),stream="fileOnly")
+        
+        # Case 2 or 3
         elif (dictionaries_list[i])['original_data'] == False:
             #search for first not-fake and determine number of fakes
             j = i
             while (dictionaries_list[j]['original_data'] == False):
                 j += 1 
-            # after 'while' lopo, j is first not-fake
+            # after 'while' loop, j is first not-fake
             # if second element after TRUE exists and if that element is also TRUE  [T T F F F F {T T}] 
-            if (j + 1 < n_entries-1 and dictionaries_list[j+1]['original_data'] == True): 
+            
+            # Case 2
+            if (j + 1 < n_entries-1 and dictionaries_list[j+1]['original_data'] == True):
                 # calculate the speed for the second original_data=True basing on first and second original_data=True and insert it into second
                 latitude_now = (dictionaries_list[j+1])['latitude_deg']
                 longitude_now = (dictionaries_list[j+1])['longitude_deg']
@@ -118,6 +130,8 @@ def calculateAndAssign(dictionaries_list):
             # if second element after FALSE is FALSE, despite first being TRUE [t f f f f {T f}]
             # then we dont have enough data points to calculate speed for "T", and simpliest thing we can do is to use average speed throughout the entire false-scope, basing on difference in coordinates between 2 real measures
             # in the future I could interpolate speeds more properly - that is: maybe look at previous true speed values and use them to calculate the speed that must had been kept over the distance that is 'fake'
+            
+            # Case 3
             elif (j + 1 < n_entries-1 and dictionaries_list[j+1]['original_data'] == False): 
                 # j is still first not-fake
                 # i is first fake
@@ -127,16 +141,22 @@ def calculateAndAssign(dictionaries_list):
                 
                 latitude_prev = (dictionaries_list[i-1])['latitude_deg']
                 longitude_prev = (dictionaries_list[i-1])['longitude_deg']
-                speeds = _calculate_speeds(latitude_prev, longitude_prev, latitude_now, longitude_now)
-
+                
+                number_of_entries = j - i + 1 # because j points at first TRUE after last fake [which does not hold instantenous speed value], and i points at first fake [which does not 
+                wrong_speeds = _calculate_speeds(latitude_prev, longitude_prev, latitude_now, longitude_now) #these are distances really, beware (it's todo)
+                correct_speeds = {key: round(value/number_of_entries, 1) for key, value in wrong_speeds.items()}
+                
                 while i <= j:
                     logger(n_entries-1,"/",i, sep='')
-                    for key in speeds:
-                        dictionaries_list[i]['speed_kmh'+'_'+key] = speeds[key]
+                    for key in correct_speeds:
+                        dictionaries_list[i]['speed_kmh'+'_'+key] = correct_speeds[key]
                     (dictionaries_list[i])['speed_source'] = "average straight line displacement [{T->(F F F F)->T F}]"
                     logger("{0}average straight line displacement [{{T->(F F F F)->T F}}]".format(tab),stream="fileOnly")
                     logger("{0}{1}".format(tab,dictionaries_list[i]['datetimeISO8601']),stream="fileOnly")
                     i += 1
                 i -= 1 #compensation - we incremented once too much and thanks to that quit 'while', but we want to increment into the next (first unchecked so far) element 
+            #else:
+            #    logger("Error in speec calculation, index + 1 is longer than number of entries (probably). Program will now quit")
+            #    quit()
         i += 1
     return dictionaries_list
